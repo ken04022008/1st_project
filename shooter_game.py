07053a472fb009bc2505131eb1d96adc98ace676
ken <keanufarrazkaimam@gmail.com>
2025-05-11 17:23:29 +0700

    name update

--- shooter_game.py
+++ shooter_game.py
@@ -6,7 +6,7 @@ font.init()
 font1 = font.Font(None, 80)
 win = font1.render('YOU WIN!', True, (255, 255, 255))
 lose = font1.render('YOU LOSE!', True, (180, 0, 0))
-
+print("name")
 
 font2 = font.Font(None, 36)
 
